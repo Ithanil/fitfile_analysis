da71b3f58358d1f7b34788b328431ef0310f251e
Jan Kessler <jakessle@t-online.de>
2021-06-15 16:39:00 +0200

    added mechanism to reset moving average computation when a time discontinuity is detected (i.e. more than 1 second diff between points)

--- parse_test.py
+++ parse_test.py
@@ -4,7 +4,7 @@ import fitparse
 from pylab import *
 
 # settings
-ma_len_0 = 1 # pseudo moving average for convenience
+ma_len_0 = 3
 ma_len_1 = 30
 ma_len_2 = 300
 ma_names = ['speed', 'power', 'heart_rate', 'cadence', 'left_right_balance']
@@ -16,37 +16,49 @@ data = {}
 mavgs_0 = {}
 mavgs_1 = {}
 mavgs_2 = {}
-cur_hist_0 = {}
-cur_hist_1 = {}
-cur_hist_2 = {}
 for name in ma_names:
     mavgs_0[name] = ([], [])
     mavgs_1[name] = ([], [])
     mavgs_2[name] = ([], [])
-    cur_hist_0[name] = np.zeros(ma_len_0)
-    cur_hist_1[name] = np.zeros(ma_len_1)
-    cur_hist_2[name] = np.zeros(ma_len_2)
-ma_it_0 = 0
-ma_it_1 = 0
-ma_it_2 = 0
 it = 0 # general iteration counter
 
 # Iterate over all messages of type "record"
 for record in fitfile.get_messages("record"):
+
     # pre-extract timestamp of the record
     for entry in record:
         if entry.name == 'timestamp':
             cur_time = entry.value
+
+    # check for discontinuity
+    time_diff = 0
+    if it > 0:
+        time_diff = cur_time - last_time
+        time_diff = time_diff.total_seconds()
+    if time_diff != 1:
+        # initialize or reset moving average computation
+        cur_hist_0 = {}
+        cur_hist_1 = {}
+        cur_hist_2 = {}
+        for name in ma_names:
+            cur_hist_0[name] = np.zeros(ma_len_0)
+            cur_hist_1[name] = np.zeros(ma_len_1)
+            cur_hist_2[name] = np.zeros(ma_len_2)
+        ma_it_0 = 0
+        ma_it_1 = 0
+        ma_it_2 = 0
+        cont_it = 0 # counter of continuous data points with 1 sec delta
+    # remember timestamp
+    last_time = cur_time
 
     # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
     for entry in record:
 
         # --- Pre-Processing of entry values ---
 
-        # skip entry if value is None
+        # convert 'None' values to 0
         if entry.value == None:
-            #data_value = 0
-            continue
+            data_value = 0
         else:
             data_value = entry.value
 
@@ -54,7 +66,7 @@ for record in fitfile.get_messages("record"):
         if entry.name == 'left_right_balance':
            # exception for weird l/r-balance values ('left'/'right') occuring at 0 power with Assiomas
             if data_value == 'left' or data_value == 'right':
-                continue
+                data_value = 50 # i.e. directly set to 50% 
             else: # convert to proper % value
                 data_value -= 128
 
@@ -68,6 +80,8 @@ for record in fitfile.get_messages("record"):
         #else:
         #    print(" * {}: {}".format(entry.name, data_value))
         #print("---")
+
+        # ----------
 
         # put desired entry data into dictionaries
         if entry.name not in data.keys():
@@ -84,13 +98,13 @@ for record in fitfile.get_messages("record"):
                     cur_hist_2[entry.name][ma_it_2] = data_value
 
                     # compute moving average value if possible
-                    if it >= ma_len_0:
+                    if cont_it >= ma_len_0:
                         mavgs_0[entry.name][0].append(cur_time)
                         mavgs_0[entry.name][1].append(sum(cur_hist_0[entry.name])/ma_len_0)
-                    if it >= ma_len_1:
+                    if cont_it >= ma_len_1:
                         mavgs_1[entry.name][0].append(cur_time)
                         mavgs_1[entry.name][1].append(sum(cur_hist_1[entry.name])/ma_len_1)
-                    if it >= ma_len_2:
+                    if cont_it >= ma_len_2:
                         mavgs_2[entry.name][0].append(cur_time)
                         mavgs_2[entry.name][1].append(sum(cur_hist_2[entry.name])/ma_len_2)
                 except:
@@ -101,10 +115,10 @@ for record in fitfile.get_messages("record"):
     ma_it_0 = (ma_it_0 + 1)%ma_len_0
     ma_it_1 = (ma_it_1 + 1)%ma_len_1
     ma_it_2 = (ma_it_2 + 1)%ma_len_2
+    cont_it += 1 # increase continuous iteration counter
     it += 1 # increase general iteration counter
 
 print(data.keys())
-print(data['timestamp'])
 
 # convert all ma data to numpy array
 for name in ma_names:
@@ -117,8 +131,8 @@ for name in ma_names:
     figure()
     title(name)
 #    plot(data['timestamp'], data[name])
-    plot(mavgs_0[name][0], mavgs_0[name][1])
-    plot(mavgs_1[name][0], mavgs_1[name][1])
-    plot(mavgs_2[name][0], mavgs_2[name][1])
+    plot(mavgs_0[name][0], mavgs_0[name][1], '*', markersize = 4)
+    plot(mavgs_1[name][0], mavgs_1[name][1], '*', markersize = 4)
+    plot(mavgs_2[name][0], mavgs_2[name][1], '*', markersize = 4)
 
 show()
