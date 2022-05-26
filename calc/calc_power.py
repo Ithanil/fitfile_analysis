5c2fe4dd988b8f23b5dcbb0dd82841a61e4588bd
Jan Kessler <jakessle@t-online.de>
2022-05-26 14:55:31 +0200

    check and properly account for time difference between data points

--- calc/calc_power.py
+++ calc/calc_power.py
@@ -15,7 +15,7 @@ def calc_direction(Alat, Along, Blat, Blong):
 
     return arctan2(X, Y)/pi*180.
 
-def calc_power(v_new, v_old, dir, slope, phys_var, verbosity = 0):
+def calc_power(v_new, v_old, tdiff, dir, slope, phys_var, verbosity = 0):
     slope_rad = arctan(slope)
     v = 0.5*(v_new + v_old)
     v_wind = v - phys_var['wind_v'] * cos((dir - phys_var['wind_dir'])/180.*pi)
@@ -25,7 +25,7 @@ def calc_power(v_new, v_old, dir, slope, phys_var, verbosity = 0):
     F_w = 0.5 * phys_var['cda'] * phys_var['rho'] * v_wind*v_wind
 
     pow_base = (F_g + F_r + F_w) * v
-    pow_acc = 0.5*phys_var['mass']*(v_new**2 - v_old**2)
+    pow_acc = 0.5*phys_var['mass']*(v_new**2 - v_old**2) / tdiff
 
     if verbosity > 1:
         print(F_g, F_r, F_w)
@@ -54,9 +54,13 @@ def calc_power_data(data, phys_var, calc_neg_watts = True, verbosity = 0):
         v_new = data['speed'][it]/3.6
         if it > 0:
             dir = calc_direction(data['position_lat'][it-1], data['position_long'][it-1], data['position_lat'][it], data['position_long'][it])
+            tdiff = data['seconds'][it] - data['seconds'][it-1]
+            if tdiff < 1:
+                print('Error: Time advancement between points was negative!')
         else:
             dir = phys_var['wind_dir'] + 90. # just assume perfect cross wind for first iteration
-        pow = calc_power(v_new, v_old, dir, data['slope'][it], phys_var, verbosity)
+            tdiff = 1
+        pow = calc_power(v_new, v_old, tdiff, dir, data['slope'][it], phys_var, verbosity)
 #       print(data['slope'][it]*100., arctan(data['slope'][it])*180./pi)
         if pow > 0 or calc_neg_watts: # option to clamp power to positive values
             comp_pow.append(pow)
